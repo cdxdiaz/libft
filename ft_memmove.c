be58416941f514bc8233dd2e19befc9ea01a065e
Wenai Sawatwathin <wsawatwa@z3t3c3.42bangkok.com>
2022-04-25 02:34:46 +0700

    ft_memmove.c's done

---
+++ ft_memmove.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wsawatwa <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/25 02:23:21 by wsawatwa          #+#    #+#             */
+/*   Updated: 2022/04/25 02:34:26 by wsawatwa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <libft.h>
+
+void	*ft_memmove(void *dst, const void *src, size_t len)
+{
+	size_t	i;
+
+	i = 0;
+	if (dst > src)
+	{
+		while (len--)
+			((unsigned char *)dst)[len] = ((unsigned char *)src)[len];
+	}
+	else
+	{
+		ft_memcpy(dst, src, len);
+	}
+	return (dst);
+}
