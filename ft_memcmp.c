9afeb73643013498d65d7d7351daaa717ea7b034
Wenai Sawatwathin <wsawatwa@z3t3c3.42bangkok.com>
2022-05-04 21:17:31 +0700

    ft_memcmp's done

---
+++ ft_memcmp.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wsawatwa <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/04 21:11:45 by wsawatwa          #+#    #+#             */
+/*   Updated: 2022/05/04 21:16:07 by wsawatwa         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <libft.h>
+
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	while (i < n)
+	{
+		if (((unsigned char *)s1)[i] != ((unsigned char *)s2)[i])
+			return (((unsigned char *)s1)[i] - ((unsigned char *)s2)[i]);
+	i++;
+	}
+	return (0);
+}
