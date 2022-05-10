a798eccedd37cfa8859c9a8f0ab654fce25fc4e5
Wenai Sawatwathin <wsawatwa@z3t3c3.42bangkok.com>
2022-05-10 09:29:58 +0700

    ft_strdup.c's fixed norminate

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,20 +6,20 @@
 /*   By: wsawatwa <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/10 07:13:00 by wsawatwa          #+#    #+#             */
-/*   Updated: 2022/05/10 07:53:02 by wsawatwa         ###   ########.fr       */
+/*   Updated: 2022/05/10 09:21:53 by wsawatwa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <libft.h>
 
-char	*ft_strdup(char *src)
+char	*ft_strdup(char *s1)
 {
-	char	*dst;
-	size_t	i;
+	char	*s2;
+	size_t	len;
 
-	i = ft_strlen(src);
-	dst = malloc(sizeof(char) * (len + i));
-	ft_memcpy(dst, src, i);
-	dst[i] = 0;
-	return (dst);
+	len = ft_strlen(s1);
+	s2 = malloc(sizeof(char) * (len + 1));
+	ft_memcpy(s2, s1, len);
+	s2[i] = 0;
+	return (s2);
 }
