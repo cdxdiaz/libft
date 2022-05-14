b4a681dd88466db6ae80ce7301c38155875971ac
Wenai Sawatwathin <wsawatwa@z3t3c3.42bangkok.com>
2022-05-14 17:47:15 +0700

    ft_calloc's done'

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: wsawatwa <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 10:08:11 by wsawatwa          #+#    #+#             */
-/*   Updated: 2022/05/11 10:08:34 by wsawatwa         ###   ########.fr       */
+/*   Updated: 2022/05/14 17:45:23 by wsawatwa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,11 @@
 
 void	*ft_calloc(size_t count, size_t size)
 {
+	void	*ptr;
 
+	ptr = malloc(count * size);
+	if (ptr == NULL)
+		return (NULL);
+	ft_bzero(ptr, count * size);
+	return (ptr);
 }
