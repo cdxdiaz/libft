147dabb7f2369672dfb4acbad4c8d4c7c2f75d9b
Wenai Sawatwathin <wsawatwa@z3t3c3.42bangkok.com>
2022-05-16 17:12:20 +0700

    ft_strlcat's done

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,13 +6,30 @@
 /*   By: wsawatwa <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 10:05:28 by wsawatwa          #+#    #+#             */
-/*   Updated: 2022/05/11 10:06:41 by wsawatwa         ###   ########.fr       */
+/*   Updated: 2022/05/16 17:11:39 by wsawatwa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	ft_strlcat(char *restrict dst, const char * restrict src, size_t dstsize)
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
+	size_t	i;
+	size_t	len_dst;
+	size_t	len_src;
 
+	i = 0;
+	len_dst = ft_strlen(dst);
+	len_src = ft_strlen(src);
+	if (dstsize < len_dst)
+		len_src += dstsize;
+	else
+		len_src += len_dst;
+	while (src[i] != 0 && len_dst + i + 1 < dstsize)
+	{
+		dst[len_dst + i] = src[i];
+		i++;
+	}
+	dst[len_dst + i] = 0;
+	return (len_src);
 }
