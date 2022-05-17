990f0de6fb3b5c8c842e3bf4487c5427a10921db
Wenai Sawatwathin <wsawatwa@z3t3c3.42bangkok.com>
2022-05-17 07:29:48 +0700

    ft_strnstr's done

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: wsawatwa <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 10:07:14 by wsawatwa          #+#    #+#             */
-/*   Updated: 2022/05/11 10:07:53 by wsawatwa         ###   ########.fr       */
+/*   Updated: 2022/05/17 07:28:50 by wsawatwa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,18 @@
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
+	size_t	i;
+	size_t	j;
 
+	i = 0;
+	while (i < len && haystack[i] != '\0')
+	{
+		j = 0;
+		while (i + j < len && haystack[i + j] == needle[j] && needle[j] != '\0')
+			j++;
+		if (needle[j] == '\0')
+			return ((char *)haystack + i);
+		i++;
+	}
+	return (NULL);
 }
