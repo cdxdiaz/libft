dc04c2d1e371c8e6b7bcb5feead1da9027954d80
Wenai Sawatwathin <wsawatwa@z3t3c3.42bangkok.com>
2022-05-20 16:50:53 +0700

    ft_itoa's à¸หàร shorten, ft_putnbr_ft's done

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: wsawatwa <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 10:15:50 by wsawatwa          #+#    #+#             */
-/*   Updated: 2022/05/20 16:07:32 by wsawatwa         ###   ########.fr       */
+/*   Updated: 2022/05/20 16:49:12 by wsawatwa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,7 @@ char	*ft_itoa(int c)
 	}
 	else if (c >= 10)
 		str = ft_strjoin(ft_itoa(c / 10), ft_itoa(c % 10));
-	else if (c < 10 && c >= 0)
+	else
 	{
 		str[0] = c + 48;
 		str[1] = 0;
--- ft_putnbr_fd.c
+++ ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: wsawatwa <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 10:23:46 by wsawatwa          #+#    #+#             */
-/*   Updated: 2022/05/11 10:42:52 by wsawatwa         ###   ########.fr       */
+/*   Updated: 2022/05/20 16:47:37 by wsawatwa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,18 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-
+	if (n == -2147483648)
+		ft_putstr_fd("-2147483648", fd);
+	else if (n < 0)
+	{
+		ft_putchar_fd('-', fd);
+		ft_putnbr_fd(-n, fd);
+	}
+	else if (n >= 10)
+	{
+		ft_putnbr_fd(n / 10, fd);
+		ft_putchar_fd(n % 10 + 48, fd);
+	}
+	else
+		ft_putchar_fd(n + 48, fd);
 }
