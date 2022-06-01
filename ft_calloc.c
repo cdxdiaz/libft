4cca6c475425b957d78d0645b73c40c2e3c3af0b
wenai sawatwathin <diaz@wenais-MacBook-Air.local>
2022-06-02 00:13:21 +0700

    ft_calloc'fixed

--- ft_calloc.c
+++ ft_calloc.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_calloc.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: wsawatwa <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: wsawatwa <wenai.sawatwathin@gmail.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/11 10:08:11 by wsawatwa          #+#    #+#             */
-/*   Updated: 2022/05/29 04:45:49 by wsawatwa         ###   ########.fr       */
+/*   Updated: 2022/06/02 00:12:59 by wsawatwa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,10 @@ void	*ft_calloc(size_t count, size_t size)
 {
 	void	*ptr;
 
+	if (count == SIZE_MAX && size == SIZE_MAX)
+		return (0);
 	ptr = malloc(count * size);
-	if (!ptr || count == SIZE_MAX || size == SIZE_MAX)
+	if (!ptr)
 		return (0);
 	ft_bzero(ptr, count * size);
 	return (ptr);
